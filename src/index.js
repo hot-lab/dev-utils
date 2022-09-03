d8a707cb01b91781a1722d2a41d474e0e3ccf602
tamaka365 <tamaka365@gmail.com>
2022-09-03 13:17:12 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import { Box } from "./components/Box";
+// import { Button } from "./components/Button";
+
+const resolvePath = require("./utils/resolve-path");
+
+// export { Box, Button };
+
+module.exports = { resolvePath };
