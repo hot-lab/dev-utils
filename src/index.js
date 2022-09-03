a73a1525232a0d4b2b5835d486873c5ad4cdc135
Rock <tamaka365@gmail.com>
2022-09-03 15:24:07 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-// import { Box } from "./components/Box";
-// import { Button } from "./components/Button";
-
 const resolvePath = require("./utils/resolve-path");
-
-// export { Box, Button };
 
 module.exports = { resolvePath };
