3f126350abbb86dfbbf14eff5315d128854c2e8d
Rock <tamaka365@gmail.com>
2022-09-03 16:11:30 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const resolvePath = require("./utils/resolve-path");
+const requireRootModule = require("./utils/require-root-module.cjs");
 
-module.exports = { resolvePath };
+module.exports = { resolvePath, requireRootModule };
